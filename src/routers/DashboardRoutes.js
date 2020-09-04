f2602a4c446b834394439df9450e4733e568d274
Javipaur <javipaur@gmail.com>
2020-09-04 09:10:31 +0200

    Primer  commit

---
+++ src/routers/DashboardRoutes.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Navbar } from '../components/ui/Navbar'
+import { Switch, Route, Redirect } from 'react-router-dom'
+import { MarvelScreen } from '../components/marvel/MarvelScreen'
+import { HeroesScreen } from '../components/heroes/HeroesScreen'
+import { DcScreen } from '../components/dc/DcScreen'
+
+export const DashboardRoutes = () => {
+    return (
+      <>
+      <Navbar/>
+
+      <div>
+          <Switch>
+              <Route exact path="/marvel" component={MarvelScreen}></Route>
+              <Route exact path="/heroe/:heroeId" component={HeroesScreen}></Route>
+              <Route exact path="/dc" component={DcScreen}></Route>
+              
+              <Redirect to="/marvel"></Redirect>
+        </Switch>
+      </div>
+      </>
+    )
+}
